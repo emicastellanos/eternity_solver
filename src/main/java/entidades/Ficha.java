46a75a6be254586c57b156cc1c8758feaa8bbcd8
Emiliano Castellanos <castellanos.emiliano@gmail.com>
2018-03-05 22:25:35 -0300

    N5 - 20min

--- src/main/java/entidades/Ficha.java
+++ src/main/java/entidades/Ficha.java
@@ -6,18 +6,15 @@ public class Ficha {
     private int arr;
     private int der;
     private int abj;
-    private String id;
+    private int id;
 
-    public Ficha (String id){
-        this.setId(String.format("%1$3s", id).replace(' ','0'));
-    }
 
-    public Ficha(int izq, int arr,int der, int abj,String id) {
+    public Ficha(int izq, int arr,int der, int abj,int id) {
         this.izq = izq;
         this.der = der;
         this.arr = arr;
         this.abj = abj;
-        this.setId(String.format("%1$3s", id).replace(' ','0'));
+        this.id = id;
     }
 
     public int getIzq() {
@@ -52,58 +49,21 @@ public class Ficha {
         this.abj = abj;
     }
 
-    public String imprimirse(){
+    public int getId() {
+        return id;
+    }
 
-        return izq+"-"+arr+"-"+der+"-"+abj;
-    }
-    
-    public String imprimirTecho() {
-    	StringBuffer result = new StringBuffer();
-    	result.append("   ");
-    	result.append(String.format("%1$3s", arr).replace(' ','0'));
-    	result.append("   |");
-    	
-    	return result.toString();
-    }
-    
-    public String imprimirMedio() {
-    	StringBuffer result = new StringBuffer();
-    	
-    	result.append(String.format("%1$3s", izq).replace(' ','0'));
-    	result.append("   ");
-    	result.append(String.format("%1$3s", der).replace(' ','0') + "|");
-    	
-    	return result.toString();
-    }
-    
     public String imprimirAbajo() {
-    	StringBuffer result = new StringBuffer();
-    	result.append("   ");
-    	result.append(String.format("%1$3s", abj).replace(' ','0'));
-    	result.append("   |");
-    	
-    	return result.toString();
-    	
+        StringBuffer result = new StringBuffer();
+        result.append("   ");
+        result.append(String.format("%1$3s", abj).replace(' ', '0'));
+        result.append("   |");
+
+        return result.toString();
     }
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-    @Override
-    public boolean equals(Object obj) {
-        if(((Ficha) obj).getIzq()==this.getIzq() && ((Ficha) obj).getArr() ==this.getArr() && ((Ficha) obj).getDer() ==this.getDer() && ((Ficha) obj).getAbj()==this.getAbj()){
-            return true;
-        }else if(((Ficha) obj).getIzq()==this.getArr() && ((Ficha) obj).getArr() ==this.getDer() && ((Ficha) obj).getDer() ==this.getAbj() && ((Ficha) obj).getAbj()==this.getIzq()){
-            return true;
-        }else if(((Ficha) obj).getIzq()==this.getDer() && ((Ficha) obj).getArr() ==this.getAbj() && ((Ficha) obj).getDer() ==this.getIzq() && ((Ficha) obj).getAbj()==this.getArr()){
-            return true;
-        }else if(((Ficha) obj).getIzq()==this.getAbj() && ((Ficha) obj).getArr() ==this.getIzq() && ((Ficha) obj).getDer() ==this.getArr() && ((Ficha) obj).getAbj()==this.getDer()){
-            return true;
-        }else return false;
+        public void setId ( int id){
+            this.id = id;
+        }
     }
-}
+
---
+++ src/main/java/soluciones/backtracking/HilosMain.java
@@ -0,0 +1,60 @@
+package soluciones.backtracking;
+
+import entidades.Ficha;
+import entidades.Tablero;
+import soluciones.master_slave.TareaRunnable;
+
+import org.apache.log4j.Logger;
+import utilidades.GeneradorFichas;
+
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class HilosMain {
+    public static ArrayList<Ficha> fichas;
+
+    private static final int N = 5;
+    static final Logger fichasLog = Logger.getLogger("fichasLogger");
+    static final Logger resultLog = Logger.getLogger("resultadoLogger");
+
+
+    public static void main(String[] args) {
+        GeneradorFichas generadorFichas = new GeneradorFichas(N);
+        fichas = generadorFichas.getFichasUnicas();
+    
+        
+
+        Collections.shuffle(fichas);
+        fichasLog.info("-----Fichas Generadas-----");
+
+        List<TareaRunnable> tareas = new ArrayList<TareaRunnable>();
+        int nhilo= 1;
+        Ficha auxF;
+        for (Ficha f : fichas) {
+            Tablero auxTablero = new Tablero(N);
+            auxF = new Ficha(f.getIzq(),f.getArr(),f.getDer(),f.getAbj(),f.getId());
+            auxTablero.insertarFinal(auxF);
+            if(auxTablero.esSolucion()){
+                ArrayList<Ficha> aux = new ArrayList<Ficha>();
+                for (Ficha e : fichas) {
+                    if (e.getId() != f.getId()) {
+                        aux.add(e);
+                    }
+                }
+                TareaRunnable tarea = new TareaRunnable(auxTablero,aux,String.valueOf(nhilo));
+                nhilo++;
+                tareas.add(tarea);
+            }
+        }
+        ZonedDateTime zdt ;
+        resultLog.info("INICIO " + ZonedDateTime.now());
+        for(TareaRunnable t :tareas)
+        	t.start();     
+        
+
+    }
+}
